1d56eb32233dc80b2703146fb80f58ead61fff50
May Nicochelli <mayaranicochelli@gmail.com>
2024-08-25 22:53:05 -0300

    [Pet Adoption Center] feat: Add new API methods

--- server.js
+++ server.js
@@ -1,25 +1,63 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json)
 
-const pets = []
+app.post('/pets', async (req, res) => {
 
-app.post('/pets', (req, res) => {
-
-    pets.push(req.body)
+    await prisma.pet.create({
+        data: {
+            name: req.body.name
+        }
+    })
 
     // Response status + resource created
     res.status(201).json(req.body)
 })
 
-app.get('/pets', (req, res) => {
-    
+app.get('/pets', async (req, res) => {
+
+    const pets = []
+
+    if(req.query){
+        // Filter
+        pets = await prisma.pet.findMany({
+            where: {
+                name: req.query.name
+            }
+        })
+    } else {
+        pets = await prisma.pet.findMany()
+    }
+
     res.status(200).json(pets)
 })
 
+app.put('/pets/:id', async (req, res) => {
+
+    await prisma.pet.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            name: req.body.name
+        }
+    })
+
+    res.status(201).json(req.body)
+})
+
+app.delete('/pets/:id', async (req, res) => {
+    await prisma.pet.delete({
+        where: {
+            id: req.params.id
+        }
+    })
+
+    res.status(200).json({ message: 'pet deleted'})
+})
+
 app.listen(3000)
-
-app.put('/pets')
-
-app.delete('/pets')
